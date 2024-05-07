efef1d2291456b3f44eddde961becab52cb41e7c
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-07 17:20:14 +0530

    gamemanager.ts

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,19 +1,30 @@
+import { WebSocket } from "ws";
 
 export class GameManager {
     private games: Game[];
-
+private pendingUser:WebSocket;
+private users:WebSocket;
 
     // user,game
     constructor() {
         this.games = [];
     } 
     addUser(socket: WebSocket) {
+        this.users.push(socket);
+        this.addHandler(socket);
         
     }
     removeUser(socket: WebSocket) {
-        
+        this.users = this.users.filter(user => user != socket);
+        // stop the game here bcsz the user left
     } 
-    private handleMessage() {
+    private addHandler(socket:WebSocket) {
+        socket.on("message",(data)=>{
+            const message =  JSON.parse(data.toString());
+            if(message.type === "init_game"){
+                this.joinGame(socket);
+            }
+        })
        
     }
 }
