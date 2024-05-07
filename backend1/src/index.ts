3bf669fb5d527b64a80a734358c289b751abe19f
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-07 14:40:48 +0530

    connect to ws

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,1 +1,14 @@
 // ws in Node js 
+import WebSocket from 'ws';
+
+const ws = new WebSocket('ws://www.host.com/path');
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  ws.send('something');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+});
