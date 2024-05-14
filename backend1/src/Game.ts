a62866bc6c59fae4574b9296eabdce96e1deb84c
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-14 12:22:24 +0530

    update the board and push to move

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,23 +1,83 @@
 
 import {WebSocket} from "ws";
+import { GAMEOVER, INIT_GAME, MOVE } from "./Message";
+
+
 export class Game {
   public player1:WebSocket;
     public player2:WebSocket;
-private board : string;
-private moves : string[];
+private board : Chess;
 private startTime : Date;
 
     constructor(player1:WebSocket ,player2:WebSocket){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.moves = [];
+        this.board = new Chess();
+       
         this.startTime = new Date();
+        this.player1.send(JSON.stringify({
+            type:INIT_GAME,
+            payload:{
+                color:'white' 
+            }
+        }));
+        this.player2.send(JSON.stringify({
+            type:INIT_GAME,
+            payload:{
+                color:'black' 
+            }
+        }));
     }
-    makeMove(socket:WebSocket,move:string){
-// validation here
-// is this user move
-// is the move valid
+    makeMove(socket:WebSocket,move:{
+        from:string;
+to:string;
+    }){
+
+
+        if(this.board.moves.length % 2 === 0 && socket != this.player1){
+            return
+        }
+        if(this.board.moves.length % 2=== 1 && socket != this.player2){
+            return
+        }
+        // validate the tupe of move using zod
+
+try{
+    this.board.move(move)
+}catch(e){
+    console.log(e);
+}
+if(this.board.isGameover()){
+// send the message to player 1
+this.player1.emit(JSON.stringify({
+type:GAMEOVER,
+payload:{
+    winner:this.board.turn() === 'w'?'black':'white'
+}
+}))
+this.player2.emit(JSON.stringify({
+    type:GAMEOVER,
+    payload:{
+        winner:this.board.turn() === 'w'?'black':'white'
+    }
+    }))
+return;
+}
+
+if(this.board.moves.length % 2 === 0){
+    this.player2.emit(JSON.stringify({
+        type:MOVE,
+        payload:move
+    }))
+   
+}
+else{
+      this.player1.emit(JSON.stringify({
+        type:MOVE,
+        payload:move
+      }))  
+}
+
 // update the  board
 // push the move
 // check if the game is over    
